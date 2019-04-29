2111382fc80bcfe82187083fac32bc54b0bb7b12
Vasileios Charatsidis <charatsidisvasileios@gmail.com>
2019-04-29 12:08:07 +0200

    almost working

--- part1/vanilla_rnn.py
+++ part1/vanilla_rnn.py
@@ -1,3 +1,4 @@
+
 ################################################################################
 # MIT License
 #
@@ -21,6 +22,7 @@ from __future__ import print_function
 import torch
 import torch.nn as nn
 
+
 ################################################################################
 
 class VanillaRNN(nn.Module):
@@ -28,63 +30,47 @@ class VanillaRNN(nn.Module):
     def __init__(self, seq_length, input_dim, num_hidden, num_classes, batch_size, device='cpu'):
         super(VanillaRNN, self).__init__()
         # Initialization here ...
-        self.W_hx = torch.nn.Parameter(torch.zeros(num_hidden, num_classes))
-        self.W_hh = nn.Parameter(torch.zeros(num_hidden, num_hidden))
+        self.W_hx = torch.nn.Parameter(torch.zeros(num_hidden, seq_length))
+        torch.nn.init.xavier_uniform_(self.W_hx)
+        self.W_hh = nn.Parameter(torch.zeros(num_hidden, num_classes))
+        torch.nn.init.xavier_uniform_(self.W_hh)
         self.b_h = nn.Parameter(torch.zeros(num_hidden))
 
         self.W_ph = torch.nn.Parameter(torch.zeros(num_classes, num_hidden))
+        torch.nn.init.xavier_uniform_(self.W_ph)
         self.b_p = torch.nn.Parameter(torch.zeros(num_classes))
 
         self.seq_length = seq_length
         self.batch_size = batch_size
         self.num_hidden = num_hidden
         self.device = device
+        self.num_classes = num_classes
 
     def forward(self, x):
         # Implementation here ...
-        y_prev = torch.zeros(self.num_hidden, self.batch_size)
+        y_prev = torch.zeros(self.num_hidden, self.num_classes)
 
         for i in range(self.seq_length):
-            print("Whx shape")
-            print(self.W_hx.shape)
-            print("x shape")
-            print(x.shape)
-            print("")
 
+            a = torch.mm(self.W_hx, x.transpose(1, 0))
 
-            #x = x[:, i].view(1, -1)
-            a = torch.mm(self.W_hx, x.transpose(1,0))
+            b = torch.mm(self.W_hh, y_prev.transpose(1, 0))
 
-            print("Whh shape")
-            print(self.W_hh.shape)
-            print("y_prev shape")
-            print(y_prev.shape)
-            print("b_h shapes")
-            print(self.b_h.shape)
-            b = torch.mm(self.W_hh, y_prev)
-
-            print("b shape")
-            print(b.shape)
-            print("a shape")
-            print(a.shape)
             linear = a + b
 
             linear2 = linear + self.b_h
-            print("linear shape")
-            print(linear2.shape)
+
             tanh = torch.nn.Tanh()
             h = tanh(linear2)
 
-            #p = h.transpose(1, 0) @ self.W_ph.transpose(1, 0) + self.b_p  # (h, b)T (o, h)T  + (o) = (b, o)
-
             p = torch.mm(self.W_ph, h).transpose(1, 0) + self.b_p
 
-            print("p shape")
-            print(p.shape)
             softmax = torch.nn.Softmax()
             y = softmax(p)
 
             y_prev = y
 
-
         return y_prev
+
+
+
